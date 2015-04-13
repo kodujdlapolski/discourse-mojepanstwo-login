e87707ac02b898beea8acd849a84ac46c206035a
root <krzysztof@madejscy.pl>
2015-04-13 13:42:31 +0000

    OAuth2Authenticator

--- plugin.rb
+++ plugin.rb
@@ -5,44 +5,9 @@
 # url: https://github.com/kodujdlapolski/discourse-mojepanstwo-login
 
 gem 'omniauth-mojepanstwo-oauth2', '0.4.0'
+require 'auth/oauth2_authenticator'
 
-class MojepanstwoAuthenticator < ::Auth::Authenticator
-
-  def name
-    'mojepanstwo'
-  end
-
-  def after_authenticate(auth_token)
-    result = Auth::Result.new
-
-    data = auth_token[:info]
-    email = auth_token[:extra][:email]
-    raw_info = auth_token[:extra][:raw_info]
-#	Trusing email, not using uid
-#    weibo_uid = auth_token[:uid]
-
-#    current_info = ::PluginStore.get('mojepanstwo', "mojepanstwo_uid_#{weibo_uid}")
-
-#    result.user =
-#      if current_info
-#        User.where(id: current_info[:user_id]).first
-#      end
-
-    result.name = data['name']
-    #result.username = data['name']
-    result.email = email
-    result.extra_data = { 
-#	weibo_uid: weibo_uid, 
-	raw_info: raw_info 
-    }
-
-    result
-  end
-
-#  def after_create_account(user, auth)
-#    weibo_uid = auth[:extra_data][:uid]
-#    ::PluginStore.set('mojepanstwo', "weibo_uid_#{weibo_uid}", {user_id: user.id})
-#  end
+class MojepanstwoAuthenticator < ::Auth::OAuth2Authenticator
 
   def register_middleware(omniauth)
     omniauth.provider :mojepanstwo, :setup => lambda { |env|
@@ -55,7 +20,7 @@ end
 
 auth_provider :frame_width => 920,
               :frame_height => 800,
-              :authenticator => MojepanstwoAuthenticator.new,
+              :authenticator => MojepanstwoAuthenticator.new('mojepanstwo'),
               :background_color => 'rgb(230, 22, 45)'
 
 register_css <<CSS
